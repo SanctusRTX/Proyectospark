7a39fa3440ac635e0ae0d1a4646064bf56ca947a
SanctusRTX <sanctusrtx@gmail.com>
2025-03-13 18:10:26 -0400

    version 0.6

--- src/app.py
+++ src/app.py
@@ -1,12 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect,url_for,flash
+from flask_mysqldb import MySQL
 from config import config
+from models.ModelUser import ModelUser
+from models.entities.User import User
+
 
 app = Flask(__name__)
 
-@app.route('/')    
+app.config.from_object('config.Config')
+
+db = MySQL(app)
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])    
 def login():
+    if request.method == 'POST':
+        user = User(0, request.form['username'], request.form['password'])
+        logged_user = ModelUser.login(db,user)
+        if logged_user != None:
+            if logged_user.password:
+                return redirect(url_for('home'))
+            else:
+                flash('Contraseña incorrecta...')
+                return render_template('auth/login.html')             
+        else:
+            flash('usuario no encontrado...')
+            return render_template('auth/login.html')
+    else:
+        return render_template('auth/login.html')
     
-    return render_template('auth/login.html')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
--- src/config.py
+++ src/config.py
@@ -1,4 +1,15 @@
-class developmentconfig():
-    DEBUG = True 
-config={
-    'development': developmentconfig}
+class Config:
+    SECRET_KEY = 'Tutankamon'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'proyectospark'
+
+
+config = {
+    'development': DevelopmentConfig
+}
